28934e8df8c4b8677f5e1d6d88c31e349a734623
Gabriel Itzcovichs <gabrielitzco@gmail.com>
2020-06-22 20:28:43 -0300

    agregue cliente

--- MESA5_Practica/src/main/java/Entidades/Cliente.java
+++ MESA5_Practica/src/main/java/Entidades/Cliente.java
@@ -10,5 +10,32 @@ package Entidades;
  * @author gabri
  */
 public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String DNI;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
     
 }
